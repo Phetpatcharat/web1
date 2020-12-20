33466f965fdc6758fcb1886d2d0f4d86b25ddaea
Phetpatcharat <76037179+Phetpatcharat@users.noreply.github.com>
2020-12-20 22:18:19 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,47 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <mata charset = "UTF-8">
         <title>Phetcharat</title>
     </head>
 
-<body>
+    <body bgcolor = "#F8F8FF">
+        <table width = "700px" height="800px" align="center">
+            <tr>
+                <td width = "200px" bgcolor="#FF7F50" style="color: azure; padding: 15px">
+                    <img src = "img4.2.jpg" style="border-radius: 50%;width:135px;height:160px;vertical-align:top;margin:0px 50px"> 
+                    <h1>About Me</h1><hr>
+                    <p>
+                        <b>Name : </b>Phetcharat Suk-ubon<br><br>
+                        <b>Nickname : </b>Phet<br><br>
+                        <b>Birthday : </b>Aug 31,2000<br><br>
+                        
+                        <b>Address : </b><br>
+                           19/7 ถนนต้นโพธิ์ ตำบลขันเงิน
+                           อำเภอหลังสวน จังหวัดชุมพร<br><br>
+                        <b>Mobile : </b>0980291600<br><br>
+                        <b>Email : </b><br>sukubon_p@silpakorn.edu<br><br>
+                        <b>ID Line :</b> patcharat_p<br><br>
+                        <b>IG : </b> phet_charatt<br><br>
+                        <b>Facebook : </b> Phet Patcharat<br>
 
-    <h1>Resume</h1>
-    <p>นางสาวเพชรัตน์ สุขอุบล</p>
+                    </p>
+                
+                </td>
+                <td width = "500px" bgcolor="#FFF8DC">
+                    <h1> EDUCATION</h1>
+                    <dl>
+                        <dt>Bachelor of Science </dt>
+                        <dd>-Silpakorn University</dd>
+                        <dt>Secondary School</dt>
+                        <dd>-Suansriwittaya School</dd>
 
-</body>
+                      </dl>
+                      
+                </td>
+            </tr>
+        
+        </table>
+    </body>
+
 </html>
